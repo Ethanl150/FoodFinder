4eb29761c18b4c109d71fe6c8f00170ebff08804
Ethan Lacy <ethanr0m818@gmail.com>
2020-02-20 18:26:27 -0500

    added limit for searches and style to results

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 $("#searchB").on("click", function (event) {
   event.preventDefault();
   $("#results").empty();
-
   var foodType = $(".expand-search").val().toLowerCase();
-  var queryURL = "https://developers.zomato.com/api/v2.1/search?q=" + foodType + "&lat=39.9526&lon=-75.1652&radius=1000&apikey=14ff5e7acc0cefd1cb956054c40f30fc"
+  var limit = $("#limit").val();
+  var queryURL = "https://developers.zomato.com/api/v2.1/search?q=" + foodType + "&count=" + limit + "&lat=39.9526&lon=-75.1652&radius=1000&apikey=14ff5e7acc0cefd1cb956054c40f30fc"
 
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function (response) {
-    console.log(response)
-    for (i = 0; i < 10; i++) {
+    for (i = 0; i < limit; i++) {
       var newDiv = $("<div>").css("border-bottom", "2px solid #b3aead")
       var title = $("<a href=" + response.restaurants[i].restaurant.url + " target='_blank'><h1>" + response.restaurants[i].restaurant.name + "</h1></a>")
       title.css("text-decoration", "underline")
       var cuisine = $("<p>" + response.restaurants[i].restaurant.cuisines + "</p>");
       cuisine.css("font-style", "italic");
-      var address = $("<p>Address: " + response.restaurants[i].restaurant.location.address + "</p>")
+      cuisine.css("color", "orange");
+      var address = $("<p><b>Address:</b> " + response.restaurants[i].restaurant.location.address + "</p>")
       address.css("font-style", "italic");
-      var rating = $("<p>Rating: " + response.restaurants[i].restaurant.user_rating.aggregate_rating + "/5 &#11088;</p>")
+      var rating = $("<p><b>Rating:</b> " + response.restaurants[i].restaurant.user_rating.aggregate_rating + "/5 &#11088;</p>")
 
       if (response.restaurants[i].restaurant.user_rating.rating_text == "Not rated") {
-        rating = $("<p>Rating: N/A</p>")
+        rating = $("<p><b>Rating:</b> N/A</p>")
       }
 
       newDiv.append(title, cuisine, address, rating)
